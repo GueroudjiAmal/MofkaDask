b862d22dc6011d5fbc889174129b7a88b9d15eb0
GueroudjiAmal <ea_gueroudji@esi.dz>
2024-04-04 23:56:45 +0000

    updated script

--- scripts/polaris.sh
+++ scripts/polaris.sh
@@ -14,6 +14,9 @@ SCHEFILE=scheduler.json
 CONFIGFILE=config.json
 DCONFIGFILE=config.txt
 PROTOCOL=na+sm
+NDEPTH=12
+export DXT_ENABLE_IO_TRACE=1
+export DARSHAN_LOG_DIR_PATH=$PBS_O_WORKDIR
 
 # Split nodes between the different steps
 total=$(wc -l $PBS_NODEFILE | awk '{print $1}')
@@ -36,15 +39,12 @@ end_5=$total
 sed -n "${start_1},${end_1}p" $PBS_NODEFILE > SchedulerNode
 sed -n "${start_2},${end_2}p" $PBS_NODEFILE > ClientNode
 sed -n "${start_3},${end_3}p" $PBS_NODEFILE > WorkerNodes
-sed -n "${start_4},${end_4}p" $PBS_NODEFILE > MofkaServer
-sed -n "${start_5},${end_5}p" $PBS_NODEFILE > MofkaConsumer
+sed -n "${start_4},${end_4}p" $PBS_NODEFILE > MofkaServerNode
+sed -n "${start_5},${end_5}p" $PBS_NODEFILE > ConsumerNode
 
 echo Creating Mofka Server
 
-mpiexec  -n 1
-         --ppn 1
-         -d ${NDEPTH}
-         --hostfile MofkaServer bedrock $PROTOCOL -c $CONFIGFILE
+mpiexec  -n 1 --ppn 1 -d ${NDEPTH} --hostfile MofkaServerNode bedrock $PROTOCOL -c $CONFIGFILE &
 
 # Wait for the SSGFILE to be created
 while ! [ -f $SSGFILE ]; do
@@ -53,16 +53,7 @@ while ! [ -f $SSGFILE ]; do
 done
 
 echo launching Scheduler
-mpiexec  -n 1
-         --ppn 1
-         -d ${NDEPTH}
-         --hostfile SchedulerNode
-         --exclusive
-         --cpu-bind depth  dask scheduler --scheduler-file=$SCHEFILE
-                                          --preload MofkaSchedulerPlugin.py
-                                          --mofka-protocol=$PROTOCOL
-                                          --ssg-file=$SSGFILE
- 1>> scheduler.o  2>> scheduler.e  &
+mpiexec  -n 1 --ppn 1 -d ${NDEPTH} --hostfile SchedulerNode --exclusive  --cpu-bind depth  dask scheduler --scheduler-file=$SCHEFILE  --preload MofkaSchedulerPlugin.py  --mofka-protocol=$PROTOCOL  --ssg-file=$SSGFILE 1>> scheduler.o  2>> scheduler.e  &
 
 # Wait for the SCHEFILE to be created
 while ! [ -f $SCHEFILE ]; do
@@ -72,14 +63,7 @@ done
 
 # Connect the client to the Dask scheduler
 echo Connect Master Client
-mpiexec  -n 1
-         --ppn 1
-         -d ${NDEPTH}
-         --hostfile ClientNode
-         --exclusive
-         --cpu-bind depth  `which python` producer.py --scheduler-file=$SCHEFILE
-                                                      --mofka-protocol=$PROTOCOL
-                                                      --ssg-file=$SSGFILE 1>> client.o 2>> client.e &
+mpiexec  -n 1 --ppn 1  -d ${NDEPTH} --hostfile ClientNode  --exclusive  --cpu-bind depth  `which python` producer.py --scheduler-file=$SCHEFILE  --mofka-protocol=$PROTOCOL  --ssg-file=$SSGFILE 1>> producer.o 2>> producer.e &
 
 client_pid=$!
 
@@ -87,25 +71,11 @@ client_pid=$!
 echo Scheduler booted, Client connected, launching workers
 
 NPROC=$((NWORKERS * NRANKS))
-mpiexec  -n 1
-         --ppn 4
-         -d ${NDEPTH}
-         --hostfile WorkerNodes
-         --exclusive
-         --cpu-bind depth  dask worker  --scheduler-file=$SCHEFILE
-                                        --preload MofkaWorkerPlugin.py
-                                        --mofka-protocol=$PROTOCOL
-                                        --ssg-file=$SSGFILE 1>> worker.o 2>>worker.e  &
+mpiexec  -n 1  --ppn 4 -d ${NDEPTH} --hostfile WorkerNodes --exclusive  --cpu-bind depth  dask worker  --scheduler-file=$SCHEFILE  --preload MofkaWorkerPlugin.py  --mofka-protocol=$PROTOCOL  --ssg-file=$SSGFILE 1>> worker.o 2>>worker.e  &
 
 # Connect the Mofka consumer client
 echo Connect Mofka Client
-mpiexec  -n 1
-         --ppn 1
-         -d ${NDEPTH}
-         --hostfile ConsumerNode
-         --exclusive
-         --cpu-bind depth  `which python` consumer.py --mofka-protocol=$PROTOCOL
-                                                      --ssg-file=$SSGFILE 1>> client.o 2>> client.e &
+mpiexec  -n 1 --ppn 1  -d ${NDEPTH} --hostfile ConsumerNode --exclusive --cpu-bind depth  `which python` consumer.py --mofka-protocol=$PROTOCOL  --ssg-file=$SSGFILE 1>> consumer.o 2>> consumer.e &
 
 consumer_pid=$!
 
