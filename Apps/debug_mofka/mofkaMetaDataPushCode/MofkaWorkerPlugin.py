5176622644cb647df9913cc3003b388bfdca258a
Seth Ockerman <treewalker@polaris-login-04.hsn.cm.polaris.alcf.anl.gov>
2024-11-04 20:34:49 +0000

    move to metadata

---
+++ Apps/debug_mofka/mofkaMetaDataPushCode/MofkaWorkerPlugin.py
@@ -0,0 +1,187 @@
+import os
+import sys
+import json
+import time
+import click
+import logging
+
+from pymargo.core import Engine
+import mochi.mofka.client as mofka
+from typing import Any
+
+from distributed.diagnostics.plugin import WorkerPlugin
+
+class MofkaWorkerPlugin(WorkerPlugin):
+    """
+    MofkaWorkerPlugin is a plugin that couples Dask distributed to Mofka through the worker.
+    This plugin pushes information about the progress and state transition of Dask tasks in
+    the worker.
+    """
+    def __init__(self, worker, mofka_protocol, group_file):
+        logging.basicConfig(filename="MofkaWorkerPlugin.log",
+                            format='%(asctime)s %(message)s',
+                            datefmt='%m/%d/%Y %I:%M:%S %p',
+                            filemode='w')
+        logger = logging.getLogger()
+        logger.setLevel(logging.INFO)
+        # create mofka client
+        self.worker = worker
+        self.commin = 0
+        self.commout = 0
+        self.engine = Engine(mofka_protocol, use_progress_thread=True)
+        self.driver = mofka.MofkaDriver(group_file, self.engine)
+
+        # create a topic
+        topic_name = "Dask"
+        if not self.driver.topic_exists(topic_name):
+            logging.info("Mofka topic %s is created by MofkaPlugin", topic_name)
+            self.driver.create_topic(topic_name)
+            self.driver.add_memory_partition(topic_name, 0)
+
+        self.topic = self.driver.open_topic(topic_name)
+
+        # create a producer
+        producer_name = "Dask_worker_producer"
+        batchsize = mofka.AdaptiveBatchSize
+        thread_pool = mofka.ThreadPool(1)
+        ordering = mofka.Ordering.Strict
+        self.producer = self.topic.producer(producer_name, batchsize, thread_pool, ordering)
+        logging.info("Mofka producer %s is created", producer_name)
+
+
+    def setup(self, worker):
+        """
+        Run when the plugin is attached to a worker. This happens when the plugin is registered
+        and attached to existing workers, or when a worker is created after the plugin has been
+        registered.
+        """
+        # XXX
+        self.worker = worker
+
+    def teardown(self, worker):
+        """Run when the worker to which the plugin is attached is closed, or
+        when the plugin is removed."""
+        teardown = {"time" : "0"}
+        try:
+            f = self.producer.push({"action": "remove_worker", "time" : str(time.time())}, str(teardown).encode("utf-8"))
+            f.wait()
+            
+        except Exception as Argument:
+            logging.exception("Exception while calling remove_worker method when sending", str(teardown))
+        
+        # del self.producer
+        # del self.topic
+        # # del self.service
+        # del self.client
+        # del self.engine
+
+    def transition(
+        self,
+        key,
+        start,
+        finish,
+        **kwargs: Any,
+    ):
+        """
+        Throughout the lifecycle of a task (see :doc:`Worker State
+        <worker-state>`), Workers are instructed by the scheduler to compute
+        certain tasks, resulting in transitions in the state of each task. The
+        Worker owning the task is then notified of this state transition.
+
+        Whenever a task changes its state, this method will be called.
+
+        .. warning::
+
+            This is an advanced feature and the transition mechanism and details
+            of task states are subject to change without deprecation cycle.
+
+        Parameters
+        ----------
+        key :
+        start :
+            Start state of the transition.
+            One of waiting, ready, executing, long-running, memory, error.
+        finish :
+            Final state of the transition.
+        kwargs :
+            More options passed when transitioning
+        """
+        if "custom-placeholder-key" == str(key):
+            # for i in range(5): 
+            print("Worker flush")
+            self.producer.flush()
+            print("Worker completed flush")
+        
+        # transition_data = str({ "key" : str(key)}).encode("utf-8")
+        transition_data = str({ "key"            : str(key),
+                            "start"          : start,
+                            "finish"         : finish,
+                            "called_from"    : self.worker.name,
+                            "time"           : time.time()
+                            }).encode("utf-8")
+        try:
+            f = self.producer.push({"action": "worker_transition", "key"            : str(key),
+                            "start"          : str(start),
+                            "finish"         : str(finish),
+                            "called_from"    : str(self.worker.name),
+                            "time"           : str(time.time())}, transition_data)
+            # f.wait()
+        except Exception as Argument:
+            logging.exception("Exception while calling transition method when sending", transition_data)
+
+        l = self.commin
+        l2 = len(self.worker.transfer_incoming_log)
+        if l2 > l:
+            data = list(self.worker.transfer_incoming_log)[l-1:]
+            _ = [e.update({"type": "incoming_transfer", "called_from": self.worker.name, "time": time.time(), "keys": str(e["keys"])}) for e in data]
+            self.commin = len(self.worker.transfer_incoming_log)
+            for d in data:
+                try:
+
+                    print("Type D", type(d))
+                    print("D", d)
+                    dd = str({"start":"0"}).encode("utf-8")
+                    # print("DD: ", dd)
+                    
+                    base = {"action": "worker_transfer"}
+                    combo = base | d
+                    print("Combo: ", combo)
+                    f = self.producer.push(combo, dd)
+                    # f.wait()
+                except Exception as Argument:
+                    logging.exception("Exception while calling transition method when sending", str(d))
+
+
+        l = self.commout
+        l2 = len(self.worker.transfer_outgoing_log)
+        if l2 > l:
+            data = list(self.worker.transfer_outgoing_log)[l-1:]
+            _ = [e.update({"type": "outgoing_transfer", "called_from": self.worker.name, "time": time.time(), "keys" : str(e["keys"])}) for e in data]
+            self.commout = len(self.worker.transfer_outgoing_log)
+            for d in data:
+                try:
+                    dd = str({"start":"0"}).encode("utf-8")
+                    print("Type D", type(d))
+                    print("D", d)
+                    base = {"action": "worker_transfer"}
+                    combo = base | d
+                    print("Combo: ", combo)
+                    f = self.producer.push(combo, dd)
+                    # f.wait()
+                except Exception as Argument:
+                    logging.exception("Exception while calling transition method when sending", str(d))
+
+
+@click.command()
+@click.option('--mofka-protocol',
+                type=str,
+                default="cxi",
+                help="Mofka protocol")
+@click.option('--group-file',
+               type=str,
+               default="mofka.json",
+               help="Mofka group file path")
+
+async def dask_setup(worker, mofka_protocol, group_file):
+    plugin = MofkaWorkerPlugin(worker, mofka_protocol, group_file)
+    await worker.plugin_add(plugin)
---
+++ Apps/debug_mofka/mofkaMetaDataPushCode/consumer.py
@@ -0,0 +1,158 @@
+import os
+import sys
+import time
+
+from pymargo.core import Engine
+from pymargo.core import client as client_mode
+import mochi.mofka.client as mofka
+
+import logging
+
+import pandas as pd
+import click
+import traceback
+import json
+
+def my_data_selector(metadata, descriptor):
+    return descriptor
+
+def my_data_broker(metadata, descriptor):
+    data = bytearray(descriptor.size)
+    return [data]
+
+class MofkaConsumer():
+
+    def __init__(self, mofka_protocol, group_file):
+        logging.basicConfig(filename="MofkaConsumer.log",
+                            format='%(asctime)s %(message)s',
+                            datefmt='%m/%d/%Y %I:%M:%S %p',
+                            filemode='w')
+        logger = logging.getLogger()
+        logger.setLevel(logging.INFO)
+
+        self.engine = Engine(mofka_protocol, use_progress_thread=True)
+        self.driver = mofka.MofkaDriver(group_file, self.engine)
+        topic_name = "Dask"
+        if not self.driver.topic_exists(topic_name):
+            logging.info("Mofka topic %s is created by MofkaPlugin", topic_name)
+            self.driver.create_topic(topic_name)
+            self.driver.add_memory_partition(topic_name, 0)
+
+        self.topic = self.driver.open_topic(topic_name)
+
+        # Create a consumer
+        consumer_name = "Dask_consumer"
+        self.consumer = self.topic.consumer(name=consumer_name,
+                                batch_size=1,
+                                data_broker=my_data_broker,
+                                data_selector=my_data_selector)
+        logger.info("Mofka consumer %s is created", consumer_name)
+
+        self.scheduler_transition_rec = pd.DataFrame()
+        self.worker_transition_rec = pd.DataFrame()
+        self.worker_transfer_rec = pd.DataFrame()
+        self.client_rec = pd.DataFrame()
+        self.worker_rec = pd.DataFrame()
+        self.graph_rec = pd.DataFrame()
+        self.stop = False
+
+    def append_event_data(self, metadata , data):
+
+        if metadata["action"] == "scheduler_transition":
+            if self.scheduler_transition_rec.empty:
+                self.scheduler_transition_rec = pd.DataFrame.from_records([data])
+            else:
+                self.scheduler_transition_rec = pd.concat([self.scheduler_transition_rec,
+                                                 pd.DataFrame.from_records([data])],
+                                                 ignore_index=True)
+
+        if metadata["action"] == "worker_transition":
+            if self.worker_transition_rec.empty:
+                self.worker_transition_rec = pd.DataFrame.from_records([data])
+            else:
+                self.worker_transition_rec = pd.concat([self.worker_transition_rec,
+                                                 pd.DataFrame.from_records([data])],
+                                                 ignore_index=True)
+
+        if metadata["action"] == "worker_transfer":
+            if self.worker_transfer_rec.empty:
+                self.worker_transfer_rec = pd.DataFrame.from_records([data])
+            else:
+                self.worker_transfer_rec = pd.concat([self.worker_transfer_rec,
+                                                 pd.DataFrame.from_records([data])],
+                                                 ignore_index=True)
+
+        elif metadata["action"] == "update_graph":
+            if self.graph_rec.empty:
+                self.graph_rec = pd.DataFrame.from_records([data])
+            else:
+                self.graph_rec = pd.concat([self.graph_rec,
+                                            pd.DataFrame.from_records([data])],
+                                            ignore_index=True)
+
+        elif metadata["action"] == "remove_worker" or metadata["action"] == "add_worker" :
+            if self.worker_rec.empty:
+                self.worker_rec = pd.DataFrame.from_records([data])
+            else:
+                self.worker_rec = pd.concat([self.worker_rec,
+                                             pd.DataFrame.from_records([data])],
+                                             ignore_index=True)
+
+
+        elif metadata["action"] == "add_client" or metadata["action"] == "remove_client":
+            if self.client_rec.empty:
+                self.client_rec = pd.DataFrame.from_records([data])
+            else:
+                self.client_rec = pd.concat([self.client_rec,
+                                             pd.DataFrame.from_records([data])],
+                                             ignore_index=True)
+
+        elif metadata["action"] == "remove_client" or metadata["action"] == "close" or metadata["action"] == "before_close" : self.stop = True
+
+
+    def get_data(self):
+        while not (self.stop):
+            f = self.consumer.pull()
+            event = f.wait()
+            # data = event.data[0].decode("utf-8", "replace")
+            try:
+                # data = eval(data)
+                metadata = json.loads(event.metadata)
+                self.append_event_data(metadata, metadata)
+            except:
+                print("data failure: ", metadata, flush=True)
+                traceback.print_exc()
+                print("-------------------------")
+            finally:
+                pass
+
+    def teardown(self):
+        self.scheduler_transition_rec.to_csv("scheduler_transition.csv")
+        self.worker_transition_rec.to_csv("worker_transition.csv")
+        self.worker_transfer_rec.to_csv("worker_transfer.csv")
+        self.client_rec.to_csv("client.csv")
+        self.worker_rec.to_csv("worker.csv")
+        self.graph_rec.to_csv("graph.csv")
+
+@click.command()
+@click.option('--mofka-protocol',
+                type=str,
+                default="cxi",
+                help="Mofka protocol",)
+@click.option('--group-file',
+               type=str,
+               default="mofka.json",
+               help="Mofka group file path")
+def main(mofka_protocol, group_file):
+    t0 = time.time()
+    consumer = MofkaConsumer(mofka_protocol, group_file)
+    consumer.get_data()
+    consumer.teardown()
+    print(f"\n\nTotal time taken  = {time.time()-t0:.2f}s", flush=True)
+
+
+if __name__ == '__main__':
+    main()
+
+sys.exit(0)
+
